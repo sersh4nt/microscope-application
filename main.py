785d22576d9f5d28f8f4bfe7972c771673ef71ad
vlad <rektf12@gmail.com>
2021-01-13 13:42:02 +0400

    video stream gud
    TODO: уменьшение окна

--- main.py
+++ main.py
@@ -1,49 +1,39 @@
 import sys
 from PyQt5 import QtWidgets
-from PyQt5.QtCore import QThread, Qt, pyqtSignal, pyqtSlot
-from PyQt5.QtGui import QImage, QPixmap
-from PIL import Image
+from PyQt5.QtCore import QTimer
+from PyQt5.QtGui import QPixmap
+import qimage2ndarray
 import dessing as design
 import cv2
-
-
-class Thread(QThread):
-    changePixmap = pyqtSignal(QImage)
-
-    def run(self):
-        cap = cv2.VideoCapture(-1)
-        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
-        while True:
-            ret, frame = cap.read()
-            if ret:
-                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                maxsize = (QImage.width(), QImage.height())
-                frame.thumbnail(maxsize, Image.ANTIALIAS)
-                h, w, ch = frame.shape
-                bytes_per_line = ch * w
-                qt_format = QImage(frame.data, w, h, bytes_per_line, QImage.Format_RGB888)
-                p = qt_format.scaled(1920, 1080, Qt.KeepAspectRatio)
-                self.changePixmap.emit(p)
 
 
 class MainWindow(QtWidgets.QMainWindow, design.Ui_MainWindow):
     def __init__(self):
         super().__init__()
+        self.video_capture = cv2.VideoCapture(0)
+        self.timer = QTimer()
         self.setupUi(self)
         self.initiate_video_stream()
 
-    @pyqtSlot(QImage)
-    def set_image(self, image):
-        self.microscopeView.setPixmap(QPixmap.fromImage(image))
+    def display_frame(self):
+        w, h = self.microscopeView.size().width(), self.microscopeView.size().height()
+        ret, frame = self.video_capture.read()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame = frame[
+                (frame.shape[0] - h) // 2: (frame.shape[0] + h) // 2 - 2,
+                (frame.shape[1] - w) // 2: (frame.shape[1] + w) // 2 - 2
+        ]
+        img = qimage2ndarray.array2qimage(frame)
+        self.microscopeView.setPixmap(QPixmap.fromImage(img))
 
     def initiate_video_stream(self):
+        self.video_capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+        self.video_capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+        self.timer.timeout.connect(self.display_frame)
+        self.timer.start(1000//60)
         self.microscopeView.scaleFactor = 1.0
         self.microscopeView.imageRelativeScale = self.frameGeometry().width() / QtWidgets.QApplication.desktop().width()
         print(self.microscopeView.imageRelativeScale)
-        th = Thread(self)
-        th.changePixmap.connect(self.set_image)
-        th.start()
 
 
 def main():
